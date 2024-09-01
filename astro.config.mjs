d9422b674548434a1d358dd815c676405595b832
Siddharth Gupta <emailsiddha@gmail.com>
2024-08-31 21:19:51 -0400

    stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import lit from '@astrojs/lit';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://beenvironmental.org', // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
+	site: 'hhttps://new-bee-website.vercel.app', // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
 	sitemap: true, // Generate sitemap (set to "false" to disable)
 	integrations: [sitemap(), mdx(), lit(), icon()], // Add renderers to the config
 });
--- src/config/settings.js
+++ src/config/settings.js
@@ -1,7 +1,7 @@
 export default {
 	title: `Barrington Environmental Establishment | Student-Led Nonprofit`,
 	description: `Barrington Environmental Establishment is a student-led environmental nonprofit organization based in the East Bay Area. Our mission is to promote environmental stewardship and sustainability through community engagement, education, and advocacy.`,
-	url: `beenvironmental.org`, // No trailing slash!
+	url: `https://new-bee-website.vercel.app`, // No trailing slash!
 	name: `Barrington Environmental Establishment`, // The short name of the business or brand name. Used for things like the copyright in the footer.
 	enableThemeSwitcher: false,
 	showLittleSticksPlug: false, // Disable this if you want to remove the Little Sticks plug from the footer. (╯°□°)╯︵ ┻━┻
